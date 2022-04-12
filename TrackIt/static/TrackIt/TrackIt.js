b88a5eef9ae558a5d7af1ca7d5a3342a46a83b42
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-04-12 15:48:43 +0530

    Add currency preferences

--- TrackIt/static/TrackIt/TrackIt.js
+++ TrackIt/static/TrackIt/TrackIt.js
@@ -1,58 +1,87 @@
 // Use https://tvjs.io/ for graphs
 
-const currencies = ["AFN","ALL","BTC","DZD","USD","EUR","AOA","XCD","ARS","AMD","AWG","AUD","AZN","BSD","BHD","BDT","BBD","BYN","BZD","XOF","BMD","BTN","BOB","BAM","BWP","BRL","BND","BGN","BIF","KHR","XAF","CAD","CVE","KYD","CLP","CNY","COP","KMF","CDF","NZD","CRC","HRK","CUP","CZK","DKK","DJF","DOP","EGP","ERN","SZL","ETB","FJD","XPF","GMD","GEL","GHS","GIP","GTQ","GNF","GYD","HTG","HNL","HKD","HUF","ISK","INR","IDR","IRR","IQD","ILS","JMD","JPY","JOD","KZT","KES","KPW","KRW","KWD","KGS","LAK","LBP","LSL","LRD","LYD","CHF","MOP","MGA","MWK","MYR","MVR","MRU","MUR","MXN","MDL","MNT","MAD","MZN","MMK","NAD","NPR","ANG","NIO","NGN","MKD","NOK","OMR","PKR","PAB","PGK","PYG","PEN","PHP","PLN","QAR","RON","RUB","RWF","SHP","WST","STN","SAR","RSD","SCR","SLL","SGD","SBD","SOS","ZAR","SSP","LKR","SDG","SRD","SEK","SYP","TJS","TZS","THB","TOP","TTD","TND","TRY","TMT","UGX","UAH","AED","GBP","UYU","UZS","VUV","VES","VND","YER","ZMW","ZWL"];
+const currencies = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLP","CNY","COP","CRC","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ETB","EUR","FJD","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XCD","XOF","XPF","YER","ZAR","ZMW","ZWL"]
+const symbols = {"USD": "$", "EUR": "€", "JPY": "¥", "GBP": "£"};
+const cryptoPrices = {"BTC": "0.000", "ETH": "0.000", "SOL": "0.000", "AVAX": "0.000", "ADA": "0.000"};
+const currencyPrices = {"USD": "0.000", "EUR": "0.000", "JPY": "0.000", "GBP": "0.000", "CHF" : "0.000"};
+let intervalIds = [];
 
-// ***----Update intervals for cryptocurrency prices----***
-setInterval(() => {
-    updateCryptoPrice("BTC", "USD");
-}, 1000);
-setInterval(() => {
-    updateCryptoPrice("ETH", "USD");
-}, 1000);
-setInterval(() => {
-    updateCryptoPrice("SOL", "USD");
-}, 1000);
-setInterval(() => {
-    updateCryptoPrice("AVAX", "USD");
-}, 1000);
-setInterval(() => {
-    updateCryptoPrice("ADA", "USD");
-}, 1000);
+function clearAllIntervals() {
+    for (let i = 0; i < intervalIds.length; i++) {
+        clearInterval(intervalIds[i]);
+    }
+    intervalIds = [];
+}
 
-// ***--------------------------------------------------***
+function updateAllSymbols(preference) {
+    if (symbols[preference]) {
+        let symbol = symbols[preference];
+        document.querySelectorAll(`.pricesymbolavailable`).forEach(elm => elm.textContent = `${symbol}`);
+        document.querySelectorAll(`.pricesymbolavailable`).forEach(elm => elm.style.display = "block")
+        document.querySelectorAll(".pricesymbol").forEach(elm => elm.style.display = "none")
+    }
+    else {
+        document.querySelectorAll(".pricesymbol").forEach( elm => elm.textContent = preference);
+        document.querySelectorAll(".pricesymbol").forEach(elm => elm.style.display = "block")
+        document.querySelectorAll(`.pricesymbolavailable`).forEach(elm => elm.style.display = "none")
+    }
+}
 
-setInterval(() => {
-    updateCurrencyPrice("USD", "USD");
-}, 1000);
-setInterval(() => {
-    updateCurrencyPrice("EUR", "USD");
-}, 1000);
-setInterval(() => {
-    updateCurrencyPrice("JPY", "USD");
-}, 1000);
-setInterval(() => {
-    updateCurrencyPrice("GBP", "USD");
-}, 1000);
-setInterval(() => {
-    updateCurrencyPrice("CHF", "USD");
-}, 1000);
+function resetLocalPrices() {
+    sessionStorage.setItem("cryptoPrices", JSON.stringify(cryptoPrices));
+    sessionStorage.setItem("currencyPrices", JSON.stringify(currencyPrices));
+}
 
-let cryptoPrices = {"BTC": 0, "ETH": 0, "SOL": 0, "AVAX": 0, "ADA": 0};
-let currencyPrices = {"USD": 0, "EUR": 0, "JPY": 0, "GBP": 0, "CHF" : 0};
+async function updateAllPrices(preference) {
+    updateCryptoPrice("BTC", preference);
+    updateCryptoPrice("ETH", preference);
+    updateCryptoPrice("SOL", preference);
+    updateCryptoPrice("AVAX", preference);
+    updateCryptoPrice("ADA", preference);
+	updateCurrencyPrice("USD", preference);
+	updateCurrencyPrice("EUR", preference);
+	updateCurrencyPrice("JPY", preference);
+	updateCurrencyPrice("GBP", preference);
+	updateCurrencyPrice("CHF", preference);
+    updateAllSymbols(preference);
+    // ***----Update intervals for cryptocurrency prices----***
+    intervalIds.push(setInterval(() => {
+        updateCryptoPrice("BTC", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCryptoPrice("ETH", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCryptoPrice("SOL", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCryptoPrice("AVAX", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCryptoPrice("ADA", preference);
+    }, 1000));
+    // ***--------------------------------------------------***
+    // ***-------Update intervals for currency prices-------***
+    intervalIds.push(setInterval(() => {
+        updateCurrencyPrice("USD", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCurrencyPrice("EUR", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCurrencyPrice("JPY", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCurrencyPrice("GBP", preference);
+    }, 1000));
+    intervalIds.push(setInterval(() => {
+        updateCurrencyPrice("CHF", preference);
+    }, 1000));
+    // ***--------------------------------------------------***
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
-    localStorage.setItem("cryptoPrices", JSON.stringify(cryptoPrices));
-    localStorage.setItem("currencyPrices", JSON.stringify(currencyPrices));
-    updateCryptoPrice("BTC", "USD");
-    updateCryptoPrice("ETH", "USD");
-    updateCryptoPrice("SOL", "USD");
-    updateCryptoPrice("AVAX", "USD");
-    updateCryptoPrice("ADA", "USD");
-	updateCurrencyPrice("USD", "USD");
-	updateCurrencyPrice("EUR", "USD");
-	updateCurrencyPrice("JPY", "USD");
-	updateCurrencyPrice("GBP", "USD");
-	updateCurrencyPrice("CHF", "USD");
+    getPreference();
     const searchForm = document.querySelector(".convert form");
     searchForm.addEventListener('submit', async event => {
         event.preventDefault();
@@ -76,10 +105,22 @@ document.addEventListener('DOMContentLoaded', async () => {
         idea = await idea.json();
         document.querySelector('.idea').textContent = idea.activity;
     })
+
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    currencies.forEach(currency => {
+        const li = document.createElement('li');
+        const btn = document.createElement('button')
+        btn.textContent = currency;
+        btn.classList.add('dropdown-item');
+        btn.dataset["currency"] = currency;
+        btn.addEventListener('click', () => updatePreference(currency));
+        li.appendChild(btn);
+        dropdownMenu.appendChild(li);
+    })
 })
 
 async function getCryptoPrice(crypto, currency) {
-    let priceData = await fetch(`https://api.coinbase.com/v2/prices/${crypto}-${currency}/buy`);
+    let priceData = await fetch(`https://api.coinbase.com/v2/prices/${crypto}-${currency}/spot`);
     priceData = await priceData.json();
     return priceData.data.amount;
 }
@@ -87,35 +128,37 @@ async function getCryptoPrice(crypto, currency) {
 async function updateCryptoPrice(crypto, currency) {
     let priceData = await getCryptoPrice(crypto, currency);
 	priceData = (parseFloat(priceData).toFixed(2)).toString();
-	let prices = localStorage.getItem("cryptoPrices");
-	prices = JSON.parse(prices);
-    if (priceData !== prices[crypto]) {
-        document.querySelector(`.cryptoprice[data-${crypto}]`).textContent = `$${priceData}`;
-		percentagechange = ((parseFloat(priceData) - parseFloat(prices[crypto])) * 100/parseFloat(prices[crypto])).toFixed(3);
-		if (percentagechange == Infinity) {
+	let prices = sessionStorage.getItem("cryptoPrices");
+    prices = JSON.parse(prices);
+    if (priceData !== prices) {
+        document.querySelector(`.cryptoprice[data-${crypto}]`).textContent = `${priceData}`;
+		let cryptoPercentageChange = ((parseFloat(priceData) - parseFloat(prices[crypto])) * 100.00/parseFloat(prices[crypto])).toFixed(3);
+		if (cryptoPercentageChange == Infinity) {
 			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).textContent = "0.000%";
+            document.querySelector(`svg[data-${crypto}]`).style.color = "#eeeeee";
+			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).style.color = "#eeeeee";
 		}
 		else {
-			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).textContent = `${percentagechange}%`;
+			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).textContent = `${cryptoPercentageChange}%`;
 		}
-		if (percentagechange < 0) {
+		if (cryptoPercentageChange < 0) {
 			document.querySelector(`svg[data-${crypto}]`).classList.add("rotatesvg");
 			document.querySelector(`svg[data-${crypto}]`).style.color = "red";
 			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).style.color = "red";
 		}
-		else if (percentagechange > 0 && percentagechange != Infinity) {
+		else if (cryptoPercentageChange > 0 && cryptoPercentageChange != Infinity) {
 			document.querySelector(`svg[data-${crypto}]`).style.color = "#39FF14";
 			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).style.color = "#29EF04";
 			if (document.querySelector(`svg[data-${crypto}]`).classList.contains("rotatesvg")) {
 				document.querySelector(`svg[data-${crypto}]`).classList.remove("rotatesvg");
 			}
 		}
-		else if (percentagechange == 0) {
+		else if (cryptoPercentageChange == 0) {
 			document.querySelector(`svg[data-${crypto}]`).style.color = "#eeeeee";
 			document.querySelector(`.cryptochangepercentage[data-${crypto}]`).style.color = "#eeeeee";
 		}
 		prices[crypto] = priceData;
-        localStorage.setItem("cryptoPrices", JSON.stringify(prices));
+        sessionStorage.setItem("cryptoPrices", JSON.stringify(prices));
     }
 }
 
@@ -128,34 +171,55 @@ async function getCurrencyPrice(currency, preference) {
 async function updateCurrencyPrice(currency, preference) {
 	let priceData = await getCurrencyPrice(currency, preference);
 	priceData = (parseFloat(priceData).toFixed(2)).toString();
-	let prices = localStorage.getItem("currencyPrices");
-	prices = JSON.parse(prices);
-	if (priceData !== prices[currency]) {
-		document.querySelector(`.currencyprice[data-${currency}]`).textContent = `$${priceData}`;
-		let priceChange = ((parseFloat(priceData) - parseFloat(prices[currency])) * 100/parseFloat(prices[currency])).toFixed(3);
-		if (priceChange == Infinity) {
+	let prices = sessionStorage.getItem("currencyPrices");
+    prices = JSON.parse(prices);
+	if (priceData !== prices) {
+		document.querySelector(`.currencyprice[data-${currency}]`).textContent = `${priceData}`;
+		let currencyPercentageChange = ((parseFloat(priceData) - parseFloat(prices[currency])) * 100/parseFloat(prices[currency])).toFixed(3);
+		if (currencyPercentageChange == Infinity) {
 			document.querySelector(`.currencychangepercentage[data-${currency}]`).textContent = "0.000%";
 		}
         else {
-			document.querySelector(`.currencychangepercentage[data-${currency}]`).textContent = `${percentagechange}%`;
+			document.querySelector(`.currencychangepercentage[data-${currency}]`).textContent = `${currencyPercentageChange}%`;
 		}
-		if (priceChange < 0) {
+		if (currencyPercentageChange < 0 && currencyPercentageChange != -Infinity) {
             document.querySelector(`svg[data-${currency}]`).classList.add("rotatesvg");
 			document.querySelector(`svg[data-${currency}]`).style.color = "red";
 			document.querySelector(`.currencychangepercentage[data-${currency}]`).style.color = "red";
 		}
-		else if (priceChange > 0 && priceChange != Infinity) {
+		else if (currencyPercentageChange > 0 && currencyPercentageChange != Infinity) {
 			document.querySelector(`.currencychangepercentage[data-${currency}]`).style.color = "#29EF04";
             document.querySelector(`svg[data-${currency}]`).style.color = "#39FF14";
 			if (document.querySelector(`svg[data-${currency}]`).classList.contains("rotatesvg")) {
 				document.querySelector(`svg[data-${currency}]`).classList.remove("rotatesvg");
 			}
 		}
-		else if (priceChange == 0) {
+		else if (currencyPercentageChange == 0) {
 			document.querySelector(`svg[data-${currency}]`).style.color = "#eeeeee";
 			document.querySelector(`.currencychangepercentage[data-${currency}]`).style.color = "#eeeeee";
 		}
 		prices[currency] = priceData;
-		localStorage.setItem("currencyPrices", JSON.stringify(prices));
+		sessionStorage.setItem("currencyPrices", JSON.stringify(prices));
 	}
+}
+
+async function getPreference() {
+    let update = await fetch(`preference/`);
+    update = await update.json();
+    preference = update.currency;
+    resetLocalPrices();
+    updateAllPrices(preference);
+    return
+}
+
+async function updatePreference(preference) {
+    let update = await fetch(`preference/`, {method: "POST", body:JSON.stringify({currency: preference})});
+    update = await update.json();
+    if (update.status != 0){
+        return;
+    }
+    clearAllIntervals();
+    resetLocalPrices();
+    updateAllPrices(preference);
+    return
 }
--- TrackIt/views.py
+++ TrackIt/views.py
@@ -1,10 +1,18 @@
+from locale import currency
+import re
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.http import JsonResponse
 from TrackIt.models import User, Watchlist, currencyPreferences
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 # Create your views here.
+
+currencies = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTC","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLP","CNY","COP","CRC","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ETB","EUR","FJD","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XCD","XOF","XPF","YER","ZAR","ZMW","ZWL"]
 
 def register(request):
     if request.method == "POST":
@@ -52,3 +60,31 @@ def logout_view(request):
 
 def index(request):
     return render(request, 'TrackIt/index.html')
+
+@csrf_exempt
+def preference(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            currency = data["currency"]
+            if currency not in currencies:
+                return JsonResponse({"status": 1})    
+            user = request.user
+            if (currencyPreferences.objects.filter(user=user).count() == 0):
+                pref = currencyPreferences.objects.create(user=user, currency=currency)
+                pref.save()
+                return JsonResponse({"status": 0})
+            pref = currencyPreferences.objects.get(user=user)
+            pref.currency = currency
+            pref.save()
+            return JsonResponse({"status": 0})
+        except:
+            return JsonResponse({"status": 1})
+    try:
+        user = request.user
+        if (currencyPreferences.objects.filter(user=user).count() == 0):
+            return JsonResponse({"status": 1})
+        pref = currencyPreferences.objects.get(user=user)
+        return JsonResponse({"status": 0, "currency": pref.currency})
+    except:
+        return JsonResponse({"status": 1, "currency": "USD"})
